4a0f735448afb9cbc55e4f6da53e6df6eb0c66c7
Alejandro de Oyarbide <oyarbide_de@hotmail.com>
2023-08-10 19:24:37 +0200

    start keyboard

---
+++ src/components/key/key.tsx
@@ -0,0 +1,8 @@
+export function key() {
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1];
+  return (
+    <li>
+      <button className="key"></button>
+    </li>
+  );
+}
---
+++ src/components/keyboard/keyboard.tsx
@@ -0,0 +1,11 @@
+export function Keyboard() {
+  return (
+    <>
+      <div className="keyboard-container">
+        <ol className="keyboard">
+          <key></key>
+        </ol>
+      </div>
+    </>
+  );
+}
